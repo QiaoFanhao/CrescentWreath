7d96ab088d1b6ce7a15947bc6602842c6d7c560d
YTO <qfh8550900@gmail.com>
2026-03-17 15:08:51 +0800

    Update docs and add scripts

---
+++ RuleCore/CrescentWreath.RuleCore.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CrescentWreath.RuleCore.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ RuleCore/CrescentWreath.RuleCore/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CrescentWreath.RuleCore
+{
+    public class Class1
+    {
+
+    }
+}
